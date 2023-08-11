4a63dfefbdb55c200ed0e52da9af838b63a138e1
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-11 12:45:26 +0200

    fetching

--- app/components/ToDoList.tsx
+++ app/components/ToDoList.tsx
@@ -1,4 +1,10 @@
-function ToDoList() {
+import { ITask } from "@/types/tasks";
+
+interface ToDoListProps {
+    tasks : ITask[]
+}
+
+const ToDoList: React.FC<ToDoListProps> = ({tasks}) => {
     return (<>
         <div className="overflow-x-auto">
             <table className="table">
@@ -10,21 +16,13 @@ function ToDoList() {
                 </tr>
                 </thead>
                 <tbody>
-                {/* row 1 */}
-                <tr> 
-                    <td>Quality Control Specialist</td>
-                    <td>Blue</td>
-                </tr>
-                {/* row 2 */}
-                <tr> 
-                    <td>Desktop Support Technician</td>
-                    <td>Purple</td>
-                </tr>
-                {/* row 3 */}
-                <tr> 
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
-                </tr>
+                    {tasks.map((task) => ( 
+                        <tr key={task.id}> 
+                            <td>{task.text}</td>
+                            <td>Blue</td>
+                        </tr>
+                    ))}
+                     
                 </tbody>
             </table>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
         <h1 className="text-2xl font-bold">Todo List App</h1>
         <AddTask />
       </div>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
     </main> 
   )
 }
