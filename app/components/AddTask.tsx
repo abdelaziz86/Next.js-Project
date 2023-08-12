05d9cc0a91236f0a4af9e350bad27f4608b157f6
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-12 17:49:55 +0200

    add done

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -3,47 +3,59 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import Modal from './Modal';
 import { FormEventHandler, useState } from 'react';
 import { addTodo } from '@/api';
+import { useRouter } from "next/navigation";
+// ... (import statements)
+
 const AddTask = () => {
-    const [showModal, setShowModal] = useState(false);
+    const router = useRouter();
+    const [modalOpen, setModalOpen] = useState(false);
     const [newTaskValue, setNewTaskValue] = useState<string>('');
 
-    const handleSubmitNewToDo : FormEventHandler<HTMLFormElement> = async (e) => {
-        e.preventDefault();
+    const handleSubmitNewToDo: FormEventHandler<HTMLFormElement> = async (e) => {
+        console.log("tes"); 
+        e.preventDefault(); // Prevent the default form submission behavior
+
         try {
             const newTask = await addTodo({
-            id : "3",
-            text: newTaskValue 
-        });
+                id: "6",
+                text: newTaskValue,
+            });
             console.log("New task added:", newTask);
         } catch (error) {
             console.error("Error adding task:", error);
         }
- 
-        setNewTaskValue(""); 
-        setShowModal(false);
 
+        setNewTaskValue("");
+        setModalOpen(false);
+        router.refresh();
     }
-    
 
-    return (<div>
-        <button onClick={() => { setShowModal(true) }} className="btn btn-primary w-full">
-            Add new Task
-            <AiOutlinePlus className="ml-2" size={18} />
-        </button>
+    return (
+        <div>
+            <button onClick={() => { setModalOpen(true) }} className="btn btn-primary w-full">
+                Add new Task
+                <AiOutlinePlus className="ml-2" size={18} />
+            </button>
 
-        <Modal modalOpen={showModal} setShowModal={setShowModal}>
-            <form onSubmit={handleSubmitNewToDo}>
+            <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+                <form onSubmit={handleSubmitNewToDo}>
                 <h3 className='font-bold text-lg'>Add new task</h3>
                 <div className='modal-action'>
                     <input
-                        value={newTaskValue}
-                        onChange={e => setNewTaskValue(e.target.value)}
-                        type="text" placeholder="Type here" className="input input-bordered w-full" />
+                    value={newTaskValue}
+                    onChange={(e) => setNewTaskValue(e.target.value)}
+                    type='text'
+                    placeholder='Type here'
+                    className='input input-bordered w-full'
+                    />
+                    <button type='submit' className='btn'>
+                    Submit
+                    </button>
                 </div>
-                <button type='submit' className='btn'>Submit</button>
-            </form>
-        </Modal>
-    </div>); 
+                </form>
+            </Modal>
+        </div>
+    );
 }
 
-export default AddTask; 
+export default AddTask;
--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,39 +1,23 @@
-import React, { useEffect, useState } from "react";
-
 interface ModalProps {
-    modalOpen: boolean;
-    setShowModal: (x: boolean) => void;
-    children: React.ReactNode;
+  modalOpen: boolean;
+  setModalOpen: (open: boolean) => boolean | void;
+  children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ modalOpen, setShowModal, children }) => {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
-    if (!isClient) {
-        return null; // Return nothing on the server
-    }
-
-    return (
-        <>
-            {modalOpen && (
-                <dialog id="my_modal_3" className={`modal modal-open`}>
-                    <form method="dialog" className="modal-box">
-                        <button
-                            onClick={() => { setShowModal(false) }}
-                            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                            ✕
-                        </button>
-
-                        {children}
-                    </form>
-                </dialog>
-            )}
-        </>
-    );
+const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen, children }) => {
+  return (
+    <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
+      <div className='modal-box relative'>
+        <label
+          onClick={() => setModalOpen(false)}
+          className='btn btn-sm btn-circle absolute right-2 top-2'
+        >
+          ✕
+        </label>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
